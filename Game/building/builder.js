291783ad5798214f4ac36835bba61c36f941cf26
Kian Acaster <acasterkian@gmail.com>
2017-05-04 11:27:05 +0100

    Update builder.js

--- Game/building/builder.js
+++ Game/building/builder.js
@@ -1,7 +1,10 @@
+// Initialize variables
 var GRID_SIZE = 32;
 var cell;
 var uniqueTiles = [];
 var player;
+
+// Create a canvas, draw a player at a random position on the canvas and draw the "main" cell at 0, 0
 function setup(){
 	createCanvas(800 + GRID_SIZE + 1, 641);
 	
@@ -10,6 +13,7 @@ function setup(){
 
 }
 
+// If the game hasn't started, draw a grid on the canvas
 function draw(){
 	if(!started){
 		for(var y = 0; y < height; y += GRID_SIZE){
@@ -19,9 +23,12 @@ function draw(){
 			}
 		}
 	}	
+	
+	// Draw all the tiles from the uniqueTiles array 
 	for(var i = 0; i < uniqueTiles.length; i++){
 		uniqueTiles[i].draw();
 	}
+	// If the "main" cell isn't undefined (basically if the game hasn't started), update the cell 
 	if(cell !== undefined){
 		player.draw();
 		cell.draw();
